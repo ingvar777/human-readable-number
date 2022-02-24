2571a6913cd9f62ed891e5d37413dc7384d56171
ingvar777 <90556370+ingvar777@users.noreply.github.com>
2022-02-24 22:12:42 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
+       let temp =
+      "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(
+         " "
+      );
+   let ty = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+
+   if (number < 20) return temp[number];
+   if (number < 100)
+      return ty[Math.floor(number / 10 - 2)] + (number % 10 ? " " + temp[number % 10] : "");
+   if (number < 1000)
+      return (
+         temp[Math.floor(number / 100)] +
+         " hundred" +
+         (number % 100 == 0 ? "" : " " + toReadable(number % 100))
+      );
   
 }
